319505bb2ffb115cfcdedb1356be7557323e7888
coradi2000 <coradi_2000@hotmail.com>
2020-11-11 20:28:22 -0300

    Solucao do problema sem Interfaces

---
+++ src/model/services/RentalService.java
@@ -0,0 +1,37 @@
+package model.services;
+
+import model.entities.CarRental;
+import model.entities.Invoice;
+
+public class RentalService {
+
+	private Double pricePerDay;
+	private Double pricePerHour;
+
+	private BrazilTaxService taxService;
+
+	public RentalService(Double pricePerDay, Double pricePerHour, BrazilTaxService taxService) {
+		this.pricePerDay = pricePerDay;
+		this.pricePerHour = pricePerHour;
+		this.taxService = taxService;
+	}
+
+	public void processInvoice (CarRental carRental) {
+		long t1 = carRental.getStart().getTime(); //getTime pega o valor em milisegundos
+		long t2 = carRental.getFinish().getTime();
+		double hours = (double)(t2- t1) / 1000 / 60 / 60; // convertendo de mili p seg p min (No começo tem casting para double para garantir o resultado em double
+		
+		double basicPayment;
+		if (hours <= 12.0) {
+			basicPayment = Math.ceil(hours) * pricePerHour;
+		}
+		else {
+			basicPayment = Math.ceil(hours / 24) * pricePerDay;
+		}
+		
+		double tax = taxService.tax(basicPayment);
+		
+		carRental.setInvoice(new Invoice(basicPayment, tax));
+	}
+
+}
